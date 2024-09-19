96a04ec2efc5801a1e87b22e724febe2fe259877
ketylopess10 <ketelenluvison@icloud.com>
2024-09-19 09:38:31 -0300

    Add files via upload

---
+++ 01ex-tipodedados.py
@@ -0,0 +1,10 @@
+tipoValor = input('Digite algo')
+print('o tipo primitivo desse valor é', type(tipoValor))
+print('Só tem espaços?', tipoValor.isspace())
+print('É um número?', tipoValor.isnumeric())
+print('É alfabético?', tipoValor.isalpha())
+print('É alfanumérico?', tipoValor.isalnum())
+print('Está em maiúscula?', tipoValor.isupper())
+print('Está em minúscula?',tipoValor.islower())
+print('Está capitalizada?',tipoValor.istitle())
+
